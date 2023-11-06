c8346ad4c3cd262a10ca519a477aa203e1c95dcb
xavierrice <xavier.rice@gmail.com>
2023-11-06 12:16:14 -0500

    added home and four routes

---
+++ pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="Home">
+        <h2>Welcome to:</h2>
+        <h3>GEEK OUT:Retro Entertainment!</h3>
+      </div>
+    );
+  }
+  
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+//PAGES
+import Home from '../pages/Home.jsx'
+import FourOFour from '../pages/FourOFour.jsx'
+
+
+//COMPONENTS
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<FourOFour/>} />
+        </Routes>
+      </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
