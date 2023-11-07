93d9043cb7e86e0be5c2b1f2c339b5574706fb2c
xavierrice <xavier.rice@gmail.com>
2023-11-07 12:57:20 -0500

    added games and game

---
+++ components/Game.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const API = import.meta.env.VITE_REACT_APP_API_URL;
+
+
+export default function Game({ game }) {
+
+
+    return (
+
+        <div className="card" style={"width: 18rem"}>
+            {imgage_id ? (<img class="card-img-top" src="..." alt="Card image cap" />) : (<span>&nbsp; &nbsp; &nbsp;</span>)}
+            <div className="card-body">
+                <h3>{game.name}</h3>
+                <td>
+                    {game.is_favorite ? (
+                        <span><h3>👾</h3></span>
+                    ) : (
+                        <span>&nbsp; &nbsp; &nbsp;</span>
+                    )}
+                </td>
+            </div>
+            <div className="btn btn-primary">
+                <Link to={`/games/${game.id}`}> {game.name}</Link>
+            </div>
+        </div>
+    )
+
+}
+
--- components/Games.jsx
+++ components/Games.jsx
@@ -2,4 +2,54 @@ import { useEffect, useState } from "react";
 
 
 
-const API =  import.meta.env.VITE_REACT_APP_API_URL;
+const API = import.meta.env.VITE_REACT_APP_API_URL;
+
+function Games() {
+
+
+    const [games, setGames] = useState([])
+
+    const fetchedGames = async () => {
+        try {
+            const res = await fetch(`${API}/games`)
+            if (!res.ok) {
+                throw new Error("Network is not netWorkin'")
+            }
+            const resJson = await res.json()
+            console.log(resJson)
+            setGames(resJson)
+        } catch (err) {
+            console.error(`There was an error in the fetch.`, err)
+        }
+    }
+
+    useEffect(() => {
+        fetchedGames()
+    }, [])
+
+
+    return (
+        <div className="Games">
+            <section>
+                <table className="table-stripe table-hover">
+                    <thead>
+                        <tr>
+                           <h3>Gaming Section</h3> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {games.map((game) => {
+                            return <game key={game.id} game={game} />;
+                        })}
+                    </tbody>
+                </table>
+            </section>
+        </div>
+    );
+
+
+
+
+
+
+}
