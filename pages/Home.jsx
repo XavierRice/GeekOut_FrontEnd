a823ff440fb7883014a7fcd94d63b49e755d0243
xavierrice <xavier.rice@gmail.com>
2023-11-08 22:55:34 -0500

    styled comp home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import Header from "../components/Header";
-
+import Portal from '../src/assets/Portal.jpb'
 
 import "./Home.css"
 
 export default function Home() {
+
+  const divStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    background: `url(${portalImage}) no-repeat center center/cover`,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+
+
   const [showNavBar, setShowNavBar] = useState(false);
 
   const handleScreenClick = () => {
@@ -12,7 +27,7 @@ export default function Home() {
   };
 
   return (
-    <div className="Home1" onClick={handleScreenClick}>
+    <div className={divStyle} onClick={handleScreenClick}>
       {showNavBar && <Header />} {/* Render the NavBar component when showNavBar is true */}
     </div>
   );
