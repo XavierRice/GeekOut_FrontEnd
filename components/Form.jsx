baf62f302c9d4ed643ec084a1192dadd7f56999c
xavierrice <xavier.rice@gmail.com>
2023-11-08 21:43:35 -0500

    working on form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Form.css"
-
+import "./Form.css";
 
 const API = import.meta.env.VITE_REACT_APP_API_URL;
 
@@ -10,11 +9,11 @@ const Form = () => {
   const navigate = useNavigate();
   const [nameError, setNameError] = useState(false);
   const [costError, setCostError] = useState(false);
-  const [idError, setIdError] = useState(false)
+  const [idError, setIdError] = useState(false);
   const [game, setGame] = useState({
     name: "",
     genre: "",
-    is_favorite: true,
+    is_favorite: false,
     storyline: "",
     video_id: "",
     image_id: "",
@@ -27,186 +26,154 @@ const Form = () => {
     } else {
       setNameError(false);
     }
-    if (!game.cost ) {
+    if (!game.cost) {
       setCostError(true);
     } else {
       setCostError(false);
     }
-    if (game.video_id.length > 12){
-      setIdError(true)
-    }else{
-      setIdError(false)
+    if (game.video_id.length > 12) {
+      setIdError(true);
+    } else {
+      setIdError(false);
     }
-
   }, [game.name, game.cost, game.video_id]); //ERROR FLAGS FOR NAME AND COST
 
   const handleTextChange = (event) => {
-    
     setGame({ ...game, [event.target.id]: event.target.value });
   };
 
-  const handleFavRadio = (event) =>{
-    let value = event.target.value
-    // value = value === "True" ? true : false
-    setGame({ ...game, [event.target.id]: value });
-  }
-  
-  const addGame = async () => {
-    console.log( "game:", game)
-      axios.post(`${API}/games`, game) .then(({data})=>{ data.id ? navigate(`/games/${data.id}`) : navigate(`/games`)} ).catch((err)=>{console.log(err)})
-      // try {
-      //     const res =  await fetch(`${API}/games`, {
-      //         method: "POST",
-      //         headers: { "Content-Type": "application/json" },
-      //         body: JSON.stringify(game),
-      //       });
-      //       console.log(res)
-      //       if (!res.ok) {
-      //           throw new Error("your post ain't postin daddy!");
-      //       } else {
-      //           await res.json();
-      //           newGame.id ? navigate(`/games/${newGame.id}`) : navigate(`/games`);
-      //       }
-      //   } catch (err) {
-      //       console.error(err);
-      //   }
-    };
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addGame();
-    };
-    
-    return (
-      <div className="container d-flex justify-content-center align-items-center vh-100">
-        <div className="NewForm">
-      <h3 className="header">New Games</h3>
-      <form onSubmit={handleSubmit} className="form-group mx-auto">
-        <label htmlFor="name">Name:</label>
-        <input
-          className="form-control "
-          style={{ width: "150%" }}
-          id="name"
-          value={game.name}
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Name of Game"
-          required
-          />
-        {nameError && (
-            <div className="error-message">You Must Enter a Game title yo!</div>
-            )}
-        <label htmlFor="genre">Genre:</label>
-        <input
-          className="form-control "
-          style={{ width: "150%" }}
-          id="genre"
-          value={game.genre}
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Type of Game: First-Person-Shooter"
-          />
-        <label htmlFor="video_id">Video_id:</label>
-        <input
-          className="form-control"
-          id="video_id"
-          value={game.video_id}
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Video id?"
-          />
-           {idError && (
-            <div className="error-message">Your id must be 12 chars or less!</div>
-            )}
-        <label htmlFor="image_id">Image_id:</label>
-        <input
-          className="form-control"
-          id="image_id"
-          value={game.image_id}
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Image id?"
-          />
-        <label htmlFor="">Storyline:</label>
-        <textarea
-          className="form-control"
-          style={{ width: "190%" }}
-          id="storyline"
-          value={game.storyline}
-          rows="6"
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Tell me about the game..."
-          ></textarea>
-        <label className="inlineCheckbox1" htmlFor="is_Favorite">
-          Favorite:
-        </label>
-        <fieldset className="form-group">
-          <div className="row">
-            <legend className="col-form-label col-sm-2 pt-0"></legend>
-            <div className="col-sm-10">
-              <div className="form-check">
-                <input
-                  className="form-check-input gridRadios"
-                  type="radio"
-                 
-                  id="is_favorite"
-                  name="is_favorite"
-                  value={true}
-                  onChange={handleFavRadio}
-                ></input>
-                <label className="form-check-label" htmlFor="favoriteTrue">
-                  A FAVE!
-                </label>
-              </div>
-              <div className="form-check">
-                <input
-                  className="form-check-input gridRadios"
-                  type="radio"
-                  id="is_favorite"
-                  name="is_favorite"
-                  value={false}
+  const handleCheck = (event) => {
+    setSong({ ...game, is_favorite: !game.is_favorite })
+}
 
-                  onChange={handleFavRadio}
-                  ></input>
-                <label className="form-check-label" htmlFor="favoriteFalse">
-                  meh
-                </label>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-        <br />
-        <br />
-        <label className="form-check-label" htmlFor="cost">
-          Cost:
-        </label>
-        <input
-          className="form-control"
-          id="cost"
-          value={game.cost}
-          type="number"
-         
-          min={.01}
-          onChange={handleTextChange}
-          placeholder="How much you want for it"
-          required
+  const addGame = async () => {
+    console.log("game:", game);
+    axios
+      .post(`${API}/games`, game)
+      .then(({ data }) => {
+        data.id ? navigate(`/games/${data.id}`) : navigate(`/games`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addGame();
+  };
+
+  return (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="NewForm">
+        <h3 className="header">New Games</h3>
+        <form onSubmit={handleSubmit} className="form-group mx-auto">
+          <label htmlFor="name">Name:</label>
+          <input
+            className="form-control "
+            style={{ width: "150%" }}
+            id="name"
+            value={game.name}
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Name of Game"
+            required
           />
-        {costError && <div className="error-message">It aint free!!</div>}
+          {nameError && (
+            <div className="error-message">You Must Enter a Game title yo!</div>
+          )}
+          <label htmlFor="genre">Genre:</label>
+          <input
+            className="form-control "
+            style={{ width: "150%" }}
+            id="genre"
+            value={game.genre}
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Type of Game: First-Person-Shooter"
+          />
+          <label htmlFor="video_id">Video_id:</label>
+          <input
+            className="form-control"
+            id="video_id"
+            value={game.video_id}
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Video id?"
+          />
+          {idError && (
+            <div className="error-message">
+              Your id must be 12 chars or less!
+            </div>
+          )}
+          <label htmlFor="image_id">Image_id:</label>
+          <input
+            className="form-control"
+            id="image_id"
+            value={game.image_id}
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Image id?"
+          />
+          <label htmlFor="">Storyline:</label>
+          <textarea
+            className="form-control"
+            style={{ width: "190%" }}
+            id="storyline"
+            value={game.storyline}
+            rows="6"
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Tell me about the game..."
+          ></textarea>
+          <label className="inlineCheckbox1" htmlFor="is_Favorite">
+            Favorite:
+          </label>
+          <fieldset className="form-group">
+            <div className="row">
+              <legend className="col-form-label col-sm-2 pt-0"></legend>
+              <label className="form-check-label" htmlFor="is_Favorite">
+              </label>
+              <input
+                className="form-check-input"
+                id="is_favorite"
+                type="checkbox"
+                onChange={handleCheck}
+                checked={game.is_favorite}
+                value={game.is_favorite}
+              />
+            </div>
+          </fieldset>
+          <br />
+          <br />
+          <label className="form-check-label" htmlFor="cost">
+            Cost:
+          </label>
+          <input
+            className="form-control"
+            id="cost"
+            value={game.cost}
+            type="number"
+            min={0.01}
+            onChange={handleTextChange}
+            placeholder="How much you want for it"
+            required
+          />
+          {costError && <div className="error-message">It aint free!!</div>}
+          <br />
+          <br />
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </form>
         <br />
-        <br />
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
-      </form>
-      <br />
-      <Link to={`/games`} className="d-block text-center">
-        <button className="btn btn-secondary">Nevermind!</button>
-      </Link>
-    </div>
+        <Link to={`/games`} className="d-block text-center">
+          <button className="btn btn-secondary">Nevermind!</button>
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Form;
-
